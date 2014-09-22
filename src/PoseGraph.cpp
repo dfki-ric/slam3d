bbc566268a4e39d424c16121a4d9206fb00ad599
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2014-09-22 10:38:27 +0200

    Added PoseGraph files.

---
+++ src/PoseGraph.cpp
@@ -0,0 +1,23 @@
+#include "PoseGraph.hpp"
+
+using namespace slam3d;
+
+Node::Node()
+{
+	
+}
+
+Node::~Node()
+{
+	
+}
+
+Edge::Edge()
+{
+	
+}
+
+Edge::~Edge()
+{
+	
+}
---
+++ src/PoseGraph.hpp
@@ -0,0 +1,32 @@
+#ifndef SLAM3D_POSEGRAPH_HPP
+#define SLAM3D_POSEGRAPH_HPP
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+namespace slam3d
+{	
+	class Node
+	{
+	public:
+		Node();
+		~Node();
+		
+	private:
+		pcl::PointCloud<pcl::PointXYZ>* mPointCloud;
+		
+	};
+	
+	class Edge
+	{
+	public:
+		Edge();
+		~Edge();
+		
+	private:
+		Node* mSource;
+		Node* mTarget;
+	};
+}
+
+#endif
