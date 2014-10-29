46215f6a926e95e474db2d2c37aff51d962bdc1c
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2014-10-29 16:36:33 +0100

    Added filtering of features.

--- src/LaserOdometry.cpp
+++ src/LaserOdometry.cpp
@@ -1,9 +1,9 @@
-#include "LaserOdometry.hpp"
+#include <slam3d/LaserOdometry.hpp>
 
-#include <map>
 #include <utility> 
+#include <algorithm>
 
-// [LOAM] Zhang, J., & Singh, S. (n.d.). LOAM : Lidar Odometry and Mapping in Real-time.
+// [LOAM] Zhang, J., & Singh, S. LOAM : Lidar Odometry and Mapping in Real-time.
 
 using namespace slam3d;
 
@@ -22,7 +22,7 @@ void LaserOdometry::addScan(PointCloud::ConstPtr scan, double timestamp)
 	unsigned int cloudSize = scan->points.size();
 
 	// Calculate c-Values [LOAM: V-A (1)]
-	std::vector< std::pair<unsigned int, double> > c_values[4];
+	std::vector< std::pair<double, unsigned int> > c_values[4];
 	unsigned int sectionSize = (cloudSize - 10) / 4.0;
 	unsigned int i = 5;
 	for(unsigned int section = 0; section < 4; section++)
@@ -49,11 +49,81 @@ void LaserOdometry::addScan(PointCloud::ConstPtr scan, double timestamp)
 				+ scan->points[i + 3].z + scan->points[i + 4].z
 				+ scan->points[i + 5].z;
 
-			c_values[section].push_back(std::make_pair(i, diffX * diffX + diffY * diffY + diffZ * diffZ));
+			c_values[section].push_back(std::make_pair(diffX * diffX + diffY * diffY + diffZ * diffZ, i));
 		}
+		
+		// Sort the points based on their c-value
+		std::sort(c_values[section].begin(), c_values[section].end());
 	}
 	
-	
+	// Points flagged in this array are filtered from being used as features
+	int filter[cloudSize];
+	memset(filter, 0, sizeof(filter));
+
+	for (int i = 5; i < cloudSize - 6; i++)
+	{
+		float diffX = scan->points[i + 1].x - scan->points[i].x;
+		float diffY = scan->points[i + 1].y - scan->points[i].y;
+		float diffZ = scan->points[i + 1].z - scan->points[i].z;
+		float diff = diffX * diffX + diffY * diffY + diffZ * diffZ;
+
+		if (diff > 0.05)
+		{
+			float depth1 = sqrt(scan->points[i].x * scan->points[i].x + 
+				scan->points[i].y * scan->points[i].y +
+				scan->points[i].z * scan->points[i].z);
+
+			float depth2 = sqrt(scan->points[i + 1].x * scan->points[i + 1].x + 
+				scan->points[i + 1].y * scan->points[i + 1].y +
+				scan->points[i + 1].z * scan->points[i + 1].z);
+
+			if (depth1 > depth2)
+			{
+				diffX = scan->points[i + 1].x - scan->points[i].x * depth2 / depth1;
+				diffY = scan->points[i + 1].y - scan->points[i].y * depth2 / depth1;
+				diffZ = scan->points[i + 1].z - scan->points[i].z * depth2 / depth1;
+
+				if (sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ) / depth2 < 0.1)
+				{
+					filter[i - 5] = 1;
+					filter[i - 4] = 1;
+					filter[i - 3] = 1;
+					filter[i - 2] = 1;
+					filter[i - 1] = 1;
+					filter[i] = 1;
+				}
+			} else
+			{
+				diffX = scan->points[i + 1].x * depth1 / depth2 - scan->points[i].x;
+				diffY = scan->points[i + 1].y * depth1 / depth2 - scan->points[i].y;
+				diffZ = scan->points[i + 1].z * depth1 / depth2 - scan->points[i].z;
+
+				if (sqrt(diffX * diffX + diffY * diffY + diffZ * diffZ) / depth1 < 0.1)
+				{
+					filter[i + 1] = 1;
+					filter[i + 2] = 1;
+					filter[i + 3] = 1;
+					filter[i + 4] = 1;
+					filter[i + 5] = 1;
+					filter[i + 6] = 1;
+				}
+			}
+		}
+
+		float diffX2 = scan->points[i].x - scan->points[i - 1].x;
+		float diffY2 = scan->points[i].y - scan->points[i - 1].y;
+		float diffZ2 = scan->points[i].z - scan->points[i - 1].z;
+		float diff2 = diffX2 * diffX2 + diffY2 * diffY2 + diffZ2 * diffZ2;
+
+		float dis = scan->points[i].x * scan->points[i].x
+			+ scan->points[i].y * scan->points[i].y
+			+ scan->points[i].z * scan->points[i].z;
+
+		if (diff > (0.25 * 0.25) / (20 * 20) * dis && diff2 > (0.25 * 0.25) / (20 * 20) * dis)
+		{
+			filter[i] = 1;
+		}
+	}
 }
 
 void LaserOdometry::finishSweep()
