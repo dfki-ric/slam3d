5256674224a5b4fae3154e8ab1138c544cdc2f76
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2015-04-29 16:56:16 +0200

    Updated CMake.

--- test/dummy.cpp
+++ test/dummy.cpp
@@ -6,33 +6,27 @@ int main()
 {
 	slam::Clock clock;
 	slam::Logger logger(clock);
-	slam::G2oSolver* solver = new slam::G2oSolver(&logger);
-	slam::VertexObject v1, v2, v3;
-	v1.id = 1;
-	v2.id = 2;
-	v3.id = 3;
-	v1.corrected_pose = Eigen::Translation<double, 3>(0,0,0);
-	v2.corrected_pose = Eigen::Translation<double, 3>(0,0,0);
-	v3.corrected_pose = Eigen::Translation<double, 3>(0,0,0);
+	slam::Solver* solver = new slam::G2oSolver(&logger);
+	
+	slam::Transform pose_1(Eigen::Translation<double, 3>(0,0,0));
+	slam::Transform pose_2(Eigen::Translation<double, 3>(0,0,0));
+	slam::Transform pose_3(Eigen::Translation<double, 3>(0,0,0));
 
-	slam::EdgeObject e1,e2,e3;
-	e1.transform = Eigen::Translation<double, 3>(1,0,0);
-	e2.transform = Eigen::Translation<double, 3>(0,1,0);
-	e3.transform = Eigen::Translation<double, 3>(-0.8, -0.7, 0.1);
-	e1.covariance = slam::Covariance::Identity();
-	e2.covariance = slam::Covariance::Identity();
-	e3.covariance = slam::Covariance::Identity();
+	slam::Transform tf_1_2(Eigen::Translation<double, 3>(1,0,0));
+	slam::Transform tf_2_3(Eigen::Translation<double, 3>(0,1,0));
+	slam::Transform tf_3_1(Eigen::Translation<double, 3>(-0.8, -0.7, 0.1));
+	slam::Covariance cov = slam::Covariance::Identity();
+		
+	solver->addNode(1, pose_1);
+	solver->addNode(2, pose_2);
+	solver->addNode(3, pose_3);
 	
-	solver->addNode(v1);
-	solver->addNode(v2);
-	solver->addNode(v3);
-	
-	solver->addConstraint(e1, v1.id, v2.id);
-	solver->addConstraint(e2, v2.id, v3.id);
-	solver->addConstraint(e3, v3.id, v1.id);
+	solver->addConstraint(1,2,tf_1_2);
+	solver->addConstraint(2,3,tf_2_3);
+	solver->addConstraint(3,1,tf_3_1, cov);
 	
 	solver->saveGraph("dummy1.g2o");
-	solver->setFixed(v1.id);
+	solver->setFixed(1);
 	solver->compute();
 	solver->saveGraph("dummy2.g2o");
 	slam::IdPoseVector corr = solver->getCorrections();
