67d08c9e4a44e12178757da81a9260221ef27166
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2015-05-04 17:04:43 +0200

    Testing removeVertex()

--- test/pose_graph.cpp
+++ test/pose_graph.cpp
@@ -15,7 +15,7 @@ BOOST_AUTO_TEST_CASE(construction)
 	slam::FileLogger logger(clock, "pose_graph_1.log");
 	graph_analysis::BaseGraph::Ptr graph(new graph_analysis::lemon::DirectedGraph());
 	
-	//slam::PoseGraph graph;
+	// Create the measurements
 	slam::Measurement m1(clock.now(), "Sensor");
 	slam::Measurement m2(clock.now(), "Sensor");
 	slam::Measurement m3(clock.now(), "Sensor");
@@ -62,6 +62,23 @@ BOOST_AUTO_TEST_CASE(construction)
 	e3->transform = Eigen::Translation<double, 3>(-0.8, -0.7, 0.2);
 	graph->addEdge(e3);
 
+	slam::EdgeObject::Ptr e4(new slam::EdgeObject());
+	e4->setSourceVertex(vo3);
+	e4->setTargetVertex(vo1);
+	e4->covariance = slam::Covariance::Identity();
+	e4->transform = Eigen::Translation<double, 3>(-0.7, -0.8, 0.1);
+	graph->addEdge(e4);
+
 	// Test file output
-	graph_analysis::io::GraphIO::write("construction_test.dot", *graph, graph_analysis::representation::GRAPHVIZ);
+	graph_analysis::io::GraphIO::write("test_01.dot", *graph, graph_analysis::representation::GRAPHVIZ);
+/*	
+	// Remove a vertex
+	graph_analysis::EdgeIterator::Ptr it = graph->getEdgeIterator(vo2);
+	while(it->next())
+	{
+		graph->removeEdge(it->current());
+	}
+	graph->removeVertex(vo2);
+	graph_analysis::io::GraphIO::write("test_02.dot", *graph, graph_analysis::representation::GRAPHVIZ);
+*/
 }
