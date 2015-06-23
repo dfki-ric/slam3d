2db21f945f09dd3a5775f9fa12dc8576a46987f6
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2015-06-23 10:45:28 +0200

    Moved configuration struct to separate file.

--- include/PointCloudSensor.hpp
+++ include/PointCloudSensor.hpp
@@ -2,6 +2,7 @@
 #define SLAM_POINTCLOUDSENSOR_HPP
 
 #include "Types.hpp"
+#include "GICPConfiguration.hpp"
 #include "Sensor.hpp"
 #include "GraphMapper.hpp"
 
@@ -46,38 +47,6 @@ namespace slam
 		
 	protected:
 		PointCloud::ConstPtr mPointCloud;
-	};
-
-	/**
-	 * @class GICPConfiguration
-	 * @author Sebastian Kasperski
-	 * @date 04/17/15
-	 * @file PointCloudSensor.hpp
-	 * @brief Collection of configuration parameters for the "Generalized
-	 * Iterative Closest Point" algorithm used for alignement of collected
-	 * point clouds.
-	 */
-	struct GICPConfiguration
-	{
-		double max_correspondence_distance;
-		int maximum_iterations;
-		double transformation_epsilon;
-		double euclidean_fitness_epsilon;
-		int correspondence_randomness;
-		int maximum_optimizer_iterations;
-		double rotation_epsilon;
-		double point_cloud_density;
-		double max_fitness_score;
-		double position_sigma;
-		double orientation_sigma;
-		double max_sensor_distance;
-
-		GICPConfiguration() : max_correspondence_distance(2.5),
-		                      maximum_iterations(50), transformation_epsilon(1e-5),
-		                      euclidean_fitness_epsilon(1.0), correspondence_randomness(20),
-		                      maximum_optimizer_iterations(20), rotation_epsilon(2e-3),
-		                      point_cloud_density(0.2), max_fitness_score(2.0),
-		                      position_sigma(0.001), orientation_sigma(0.0001), max_sensor_distance(2.0) {};
 	};
 
 	/**
