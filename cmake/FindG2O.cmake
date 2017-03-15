29bc014e176672ab040a0df7eee615ce87a060af
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2017-03-15 11:07:32 +0100

    Cleaned g2o cmake file.

--- cmake/FindG2O.cmake
+++ cmake/FindG2O.cmake
@@ -1,67 +1,15 @@
 # Find the header files
-
-FIND_PATH(G2O_INCLUDE_DIR g2o/core/base_vertex.h
-  ${G2O_ROOT}/include
-  $ENV{G2O_ROOT}/include
-  $ENV{G2O_ROOT}
-  /usr/local/include
-  /usr/include
-  /opt/local/include
-  /sw/local/include
-  /sw/include
-  )
+FIND_PATH(G2O_INCLUDE_DIR g2o/core/base_vertex.h)
 
 # Macro to unify finding both the debug and release versions of the
 # libraries; this is adapted from the OpenSceneGraph FIND_LIBRARY
 # macro.
-
 MACRO(FIND_G2O_LIBRARY MYLIBRARY MYLIBRARYNAME)
 
-  FIND_LIBRARY("${MYLIBRARY}_DEBUG"
-    NAMES "g2o_${MYLIBRARYNAME}_d"
-    PATHS
-    ${G2O_ROOT}/lib/Debug
-    ${G2O_ROOT}/lib
-    $ENV{G2O_ROOT}/lib/Debug
-    $ENV{G2O_ROOT}/lib
-    )
-
-  FIND_LIBRARY("${MYLIBRARY}_DEBUG"
-    NAMES "g2o_${MYLIBRARYNAME}_d"
-    PATHS
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/local/lib64
-    /usr/lib
-    /usr/lib64
-    /opt/local/lib
-    /sw/local/lib
-    /sw/lib
-    )
-  
-  FIND_LIBRARY(${MYLIBRARY}
-    NAMES "g2o_${MYLIBRARYNAME}"
-    PATHS
-    ${G2O_ROOT}/lib/Release
-    ${G2O_ROOT}/lib
-    $ENV{G2O_ROOT}/lib/Release
-    $ENV{G2O_ROOT}/lib
-    )
-
-  FIND_LIBRARY(${MYLIBRARY}
-    NAMES "g2o_${MYLIBRARYNAME}"
-    PATHS
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/local/lib64
-    /usr/lib
-    /usr/lib64
-    /opt/local/lib
-    /sw/local/lib
-    /sw/lib
-    )
+  FIND_LIBRARY("${MYLIBRARY}_DEBUG" "g2o_${MYLIBRARYNAME}_d")
+  FIND_LIBRARY("${MYLIBRARY}_DEBUG" "g2o_${MYLIBRARYNAME}_d")
+  FIND_LIBRARY(${MYLIBRARY} "g2o_${MYLIBRARYNAME}")
+  FIND_LIBRARY(${MYLIBRARY} "g2o_${MYLIBRARYNAME}")
   
   IF(NOT ${MYLIBRARY}_DEBUG)
     IF(MYLIBRARY)
