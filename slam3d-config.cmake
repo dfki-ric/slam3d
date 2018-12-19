4cf2f5c79a91718dc36aec2c22e4c0f2a36b9d6d
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2018-12-19 14:49:15 +0100

    Removed usage of FindDependencyMacro.

--- slam3d-config.cmake
+++ slam3d-config.cmake
@@ -1,15 +1,13 @@
-include(CMakeFindDependencyMacro)
-find_dependency(Eigen3 NO_MODULE)
+find_package(Eigen3 REQUIRED)
 if (NOT TARGET Eigen3::Eigen)
-	find_dependency(Eigen3 REQUIRED)
 	add_library(Eigen3::Eigen INTERFACE IMPORTED)
 	set_property(TARGET Eigen3::Eigen APPEND PROPERTY
 		INTERFACE_INCLUDE_DIRECTORIES ${EIGEN3_INCLUDE_DIR})
 endif ()
 
-find_dependency(Boost COMPONENTS graph)
-find_dependency(PCL 1.7 COMPONENTS registration)
-find_dependency(g2o)
-find_dependency(OpenGL)
+find_package(Boost REQUIRED COMPONENTS graph)
+find_package(PCL 1.7 REQUIRED COMPONENTS registration)
+find_package(g2o REQUIRED)
+find_package(OpenGL REQUIRED)
 include("${CMAKE_CURRENT_LIST_DIR}/slam3d-targets.cmake")
 
