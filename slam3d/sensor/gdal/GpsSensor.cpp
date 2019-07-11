f83f9c1a505f82e0c830fc80464a52319b0dc1c0
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2019-07-11 16:28:38 +0200

    Changed a message's log-level.

--- slam3d/sensor/gdal/GpsSensor.cpp
+++ slam3d/sensor/gdal/GpsSensor.cpp
@@ -68,7 +68,7 @@ void GpsSensor::addMeasurement(const GpsMeasurement::Ptr &m)
 
 	mLastVertex = mMapper->addMeasurement(m);
 	Position rel_pos = m->getPosition() - mReference;
-	mLogger->message(INFO, (boost::format("GPS: relative pose (%1%, %2%, %3%)") % rel_pos(0) % rel_pos(1) % rel_pos(2)).str());
+	mLogger->message(DEBUG, (boost::format("GPS: relative pose (%1%, %2%, %3%)") % rel_pos(0) % rel_pos(1) % rel_pos(2)).str());
 	PositionConstraint::Ptr position(new PositionConstraint(mName, rel_pos, m->getCovariance()));
 	mMapper->getGraph()->addConstraint(mLastVertex, 0, position);
 }
