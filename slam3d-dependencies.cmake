9916f8a62e39e7d6a778a573e9bbd60352426f94
Pierre Willenbrock <pierre.willenbrock@dfki.de>
2022-01-17 18:04:56 +0100

    For jsoncpp, fall back to pkgconfig if cmake config fails
    i would think that jsoncpp is understood as a target, and thus should
    provide jsoncpp_lib for the cmake config version, and jsoncpp for the
    pkgconfig version.

--- slam3d-dependencies.cmake
+++ slam3d-dependencies.cmake
@@ -10,7 +10,10 @@ endif ()
 find_package(Boost REQUIRED COMPONENTS thread graph unit_test_framework)
 find_package(PCL 1.7 REQUIRED COMPONENTS registration sample_consensus)
 find_package(g2o REQUIRED)
-find_package(jsoncpp REQUIRED)
+find_package(jsoncpp)
+if (NOT jsoncpp_FOUND)
+  pkg_check_modules(jsoncpp REQUIRED IMPORTED_TARGET jsoncpp)
+endif()
 
 pkg_check_modules(flann REQUIRED IMPORTED_TARGET flann)
 
