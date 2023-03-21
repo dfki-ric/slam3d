ac517b558702ca62b159783f10eb9890cbd616f7
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2023-03-21 11:00:56 +0100

    Create measurement table.

--- slam3d/core/DataStorage.cpp
+++ slam3d/core/DataStorage.cpp
@@ -1,4 +1,5 @@
 #include "DataStorage.hpp"
+#include "Types.hpp"
 
 #include <boost/format.hpp>
 #include <pqxx/pqxx>
@@ -11,10 +12,35 @@ class slam3d::DataStorageInternal
 public:
 	DataStorageInternal(Logger* l, const std::string& sql_connect) : mLogger(l), mConnection(sql_connect){}
 	
-	void createSchema(const std::string& schema)
+	void initialize(const std::string& schema)
 	{
 		pqxx::work w(mConnection);
 		w.exec0("CREATE SCHEMA IF NOT EXISTS " + schema);
+		w.exec0("SET search_path TO " + schema + ",public");
+
+		std::string query("CREATE TABLE IF NOT EXISTS measurement(");
+		query += "id       uuid PRIMARY KEY, ";
+		query += "time     timestamp, ",
+		query += "robot    text, ";
+		query += "sensor   text, ";
+		query += "position geometry, ";
+		query += "qx       double precision, ";
+		query += "qy       double precision, ";
+		query += "qz       double precision, ";
+		query += "qw       double precision, ";
+		query += "data     bytea)";
+		w.exec0(query);
+		w.commit();
+	}
+	
+	void writeMeasurement(Measurement* m)
+	{
+		pqxx::work w(mConnection);
+		boost::format ins("INSERT INTO measurement VALUES (%1%, 'epoch' interval + %2%, %3%, %4%, %5%, %6%, %7%, %8%, %9%, %10%);");
+		pqxx::exec0((ins
+			% m->getUniqueId()
+			% m->getTimestamp().tv_sec % m->getTimestamp().tv_usec
+			% ))
 		w.commit();
 	}
 	
@@ -28,7 +54,7 @@ DataStorage::DataStorage(Logger* l, const std::string& host, const std::string& 
 {
 	boost::format conn("postgresql://slam3d:dfki-slam@%1%");
 	mInternal = new DataStorageInternal(l, (conn % mHost).str());
-	mInternal->createSchema(schema);
+	mInternal->initialize(schema);
 }
 
 DataStorage::~DataStorage()
