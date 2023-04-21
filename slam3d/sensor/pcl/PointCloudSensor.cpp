b678811eb1c202b1859d16482e5a68257e284462
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2023-04-21 19:26:01 +0200

    Implemented serialization for pointclouds.

--- slam3d/sensor/pcl/PointCloudSensor.cpp
+++ slam3d/sensor/pcl/PointCloudSensor.cpp
@@ -45,6 +45,60 @@
 
 using namespace slam3d;
 
+namespace boost {
+namespace serialization {
+
+template<class Archive>
+void serialize(Archive & ar, pcl::PCLPointCloud2 & m, const unsigned int version)
+{
+	ar & m.header;
+	ar & m.width;
+	ar & m.height;
+	ar & m.is_bigendian;
+	ar & m.is_dense;
+	ar & m.point_step;
+	ar & m.row_step;
+	ar & m.fields;
+	ar & m.data;
+}
+
+template<class Archive>
+void serialize(Archive & ar, pcl::PCLPointField & f, const unsigned int version)
+{
+	ar & f.count;
+	ar & f.datatype;
+	ar & f.name;
+	ar & f.offset;
+}
+
+template<class Archive>
+void serialize(Archive & ar, pcl::PCLHeader & h, const unsigned int version)
+{
+	ar & h.frame_id;
+	ar & h.seq;
+	ar & h.stamp;
+}
+
+} // namespace serialization
+} // namespace boost
+
+void PointCloudMeasurement::toStream(std::ostream& stream) const
+{
+	pcl::PCLPointCloud2 blob;
+	pcl::toPCLPointCloud2(*mPointCloud, blob);
+	boost::archive::text_oarchive oa(stream);
+	oa << blob;
+}
+
+void PointCloudMeasurement::fromStream(std::istream &stream)
+{
+	pcl::PCLPointCloud2 blob;
+//	stream.read (reinterpret_cast<char*> (&value), sizeof(value));
+	boost::archive::text_iarchive ia(stream);
+	ia >> blob;
+	pcl::fromPCLPointCloud2(blob, *mPointCloud);
+}
+
 PointCloudSensor::PointCloudSensor(const std::string& n, Logger* l)
  : ScanSensor(n, l)
 {
@@ -343,4 +397,10 @@ void PointCloudSensor::fillGroundPlane(PointCloud::Ptr cloud, ScalarType radius)
 			cloud->push_back(p);
 		}
 	}
+}
+
+Measurement::Ptr PointCloudSensor::createFromStream(const std::string& r, const std::string& s,
+	const Transform& p, const boost::uuids::uuid id, std::istream& stream)
+{
+	return Measurement::Ptr(new PointCloudMeasurement(r, s, p, id, stream));
 }
