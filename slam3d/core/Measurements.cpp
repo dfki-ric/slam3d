8baded244b430da7289c2fcb5f85a5f7fe631692
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2023-04-26 16:46:19 +0200

    add base class to store measurements

---
+++ slam3d/core/Measurements.cpp
@@ -0,0 +1,14 @@
+
+#include "Measurements.hpp"
+
+namespace slam3d {
+
+   void Measurements::set(const std::string& key, Measurement::Ptr measurement) {
+        measurements[key] = measurement;
+    }
+
+    Measurement::Ptr Measurements::get(const std::string& key) {
+        return measurements[key];
+    }
+
+}  // namespace slam3d
