422a2f5b401738f05cefd7979e4ff9df47704a36
Steffen Planthaber <steffen.planthaber@dfki.de>
2023-04-27 14:15:15 +0200

    move test functions to subfolder

--- slam3d/core/Measurements.hpp
+++ slam3d/core/Measurements.hpp
@@ -12,6 +12,7 @@
 #include <boost/archive/text_iarchive.hpp>
 
 #include "Types.hpp"
+#include <boost/uuid/uuid_io.hpp>
 
 namespace slam3d {
 
@@ -20,7 +21,15 @@ class Measurements {
     Measurements() {}
     virtual ~Measurements() {}
 
+    void set(const boost::uuids::uuid& uuid, Measurement::Ptr measurement) {
+        set(boost::uuids::to_string(uuid), measurement);
+    }
+
     virtual void set(const std::string& key, Measurement::Ptr measurement);
+
+    Measurement::Ptr get(const boost::uuids::uuid& uuid) {
+        return get(boost::uuids::to_string(uuid));
+    }
 
     virtual Measurement::Ptr get(const std::string& key);
 
