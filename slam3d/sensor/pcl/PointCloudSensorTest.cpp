45e09b34759aa7b8b6b8d82aade830a2d0e79902
Steffen Planthaber <steffen.planthaber@dfki.de>
2023-04-27 15:06:23 +0200

    use the MesurementSerialization class for the pcl serialization test

--- slam3d/sensor/pcl/PointCloudSensorTest.cpp
+++ slam3d/sensor/pcl/PointCloudSensorTest.cpp
@@ -3,6 +3,8 @@
 #include <boost/test/unit_test.hpp>
 #include <pcl/io/ply_io.h>
 #include <slam3d/core/FileLogger.hpp>
+#include <slam3d/core/MeasurementSerialization.hpp>
+#include <slam3d/core/test_templates/MeasurementTests.hpp>
 
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
@@ -24,6 +26,9 @@ void initEigenTransform(slam3d::Transform* mat)
 
 BOOST_AUTO_TEST_CASE(pcl_serialization)
 {
+	// register serialization handler for slam3d::PointCloudMeasurement
+	slam3d::MeasurementSerialization::registerMeasurementType<slam3d::PointCloudMeasurement>("slam3d::PointCloudMeasurement");
+
 	Clock clock;
 	FileLogger logger(clock, "pcl_sensor.log");
 	logger.setLogLevel(DEBUG);
@@ -38,47 +43,29 @@ BOOST_AUTO_TEST_CASE(pcl_serialization)
 	{
 		slam3d::Transform tf;
 		initEigenTransform(&tf);
-		PointCloudMeasurement m(pcl_cloud, ROBOT_NAME, SENSOR_NAME, tf);
-		
-		std::stringstream buffer;
-		boost::archive::text_oarchive oa(buffer);
-		oa << m;
-		//create data string with the binary data
-		std::string data = buffer.str();
+		PointCloudMeasurement::Ptr m = boost::make_shared<PointCloudMeasurement>(pcl_cloud, ROBOT_NAME, SENSOR_NAME, tf);
 
+		Measurement::Ptr m2_base = test_serialization(m);
 
-		PointCloudMeasurement::Ptr m2 (new PointCloudMeasurement());
-		std::stringstream buffer2(data);
-		boost::archive::text_iarchive ia(buffer2);
-		ia >> *(m2.get());
-
+		PointCloudMeasurement::Ptr m2 = boost::dynamic_pointer_cast<PointCloudMeasurement>(m2_base);
 		BOOST_ASSERT(m2);
-
 		if(m2)
 		{
-			BOOST_CHECK_EQUAL(m.getPointCloud()->size(), m2->getPointCloud()->size());
+			BOOST_CHECK_EQUAL(m->getPointCloud()->size(), m2->getPointCloud()->size());
 		}
 
-		BOOST_CHECK_EQUAL(m.getRobotName(), m2->getRobotName());
-		BOOST_CHECK_EQUAL(m.getTimestamp().tv_sec, m2->getTimestamp().tv_sec);
-		BOOST_CHECK_EQUAL(m.getTimestamp().tv_usec, m2->getTimestamp().tv_usec);
-		BOOST_CHECK_EQUAL(m.getSensorName(), m2->getSensorName());
-		BOOST_CHECK(m.getUniqueId() == m2->getUniqueId());
-		BOOST_CHECK(m.getSensorPose().isApprox(m2->getSensorPose()));
-		BOOST_CHECK(m.getInverseSensorPose().isApprox(m2->getInverseSensorPose()));
-		
 		// check values of subtype slam3d::PointCloudMeasurement
 		BOOST_CHECK_EQUAL(m2->getTypeName(), "slam3d::PointCloudMeasurement");
 		BOOST_CHECK_NE(m2->getPointCloud(), nullptr);
 		BOOST_CHECK_EQUAL(m2->getPointCloud()->size(), pcl_cloud->size());
 
-		BOOST_CHECK(m.getPointCloud()->sensor_origin_.isApprox(m2->getPointCloud()->sensor_origin_));
-		BOOST_CHECK(m.getPointCloud()->sensor_orientation_.isApprox(m2->getPointCloud()->sensor_orientation_));
+		BOOST_CHECK(m->getPointCloud()->sensor_origin_.isApprox(m2->getPointCloud()->sensor_origin_));
+		BOOST_CHECK(m->getPointCloud()->sensor_orientation_.isApprox(m2->getPointCloud()->sensor_orientation_));
 
 		// debug out (run with --log_level=message)
-		BOOST_TEST_MESSAGE( m.getSensorPose().matrix() );
+		BOOST_TEST_MESSAGE( m->getSensorPose().matrix() );
 		BOOST_TEST_MESSAGE( m2->getSensorPose().matrix() );
-		BOOST_TEST_MESSAGE( m.getPointCloud()->sensor_origin_.matrix() );
+		BOOST_TEST_MESSAGE( m->getPointCloud()->sensor_origin_.matrix() );
 		BOOST_TEST_MESSAGE( m2->getPointCloud()->sensor_origin_.matrix() );
 
 
