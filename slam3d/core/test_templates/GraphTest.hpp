6be13556ee600cb48796cb9930879657876215b3
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2023-05-08 11:42:37 +0200

    Use SensorName from Constraint in test.

--- slam3d/core/test_templates/GraphTest.hpp
+++ slam3d/core/test_templates/GraphTest.hpp
@@ -9,14 +9,20 @@ public:
 	const char* getTypeName() const { return "TestMeasurement"; }
 };
 
-template <class CONSTRAINT> boost::shared_ptr<CONSTRAINT> addAndGetConstraint(slam3d::Graph* graph, boost::shared_ptr<CONSTRAINT> constraint, slam3d::IdType from, slam3d::IdType to, const std::string& sensor){
+template <class CONSTRAINT> boost::shared_ptr<CONSTRAINT>
+addAndGetConstraint(slam3d::Graph* graph, boost::shared_ptr<CONSTRAINT> constraint, slam3d::IdType from, slam3d::IdType to)
+{
 	BOOST_CHECK_NO_THROW(graph->addConstraint(from, to, constraint));
 	slam3d::EdgeObject query_res;
-	BOOST_CHECK_NO_THROW(query_res = graph->getEdge(from, to, sensor));
+	BOOST_CHECK_NO_THROW(query_res = graph->getEdge(from, to, constraint->getSensorName()));
 	BOOST_CHECK_EQUAL(constraint->getType(), query_res.constraint->getType());
+	BOOST_CHECK_EQUAL(constraint->getSensorName(), query_res.constraint->getSensorName());
+
+//	Timestamps are currently not set within slam3d::Constraint, so it makes no sense to test for them.
+//	BOOST_CHECK_EQUAL(constraint->getTimestamp().tv_sec, query_res.constraint->getTimestamp().tv_sec);
+//	BOOST_CHECK_EQUAL(constraint->getTimestamp().tv_usec, query_res.constraint->getTimestamp().tv_usec);
 	return boost::dynamic_pointer_cast<CONSTRAINT>(query_res.constraint);
 }
-
 
 void addVertexToGraph(slam3d::Graph* g, slam3d::IdType exp_id, const std::string& robot, const std::string& sensor)
 {
@@ -63,19 +69,19 @@ void test_graph_construction(slam3d::Graph* graph)
 
 
 	slam3d::GravityConstraint::Ptr c3(new slam3d::GravityConstraint("S3", slam3d::Direction::Identity(), slam3d::Direction::Identity(), slam3d::Covariance<2>::Identity()));
-	slam3d::GravityConstraint::Ptr c3_res = addAndGetConstraint(graph, c3, 1, 2, "S3");
+	slam3d::GravityConstraint::Ptr c3_res = addAndGetConstraint(graph, c3, 1, 2);
 	BOOST_CHECK_EQUAL(c3->getCovariance().matrix(), c3_res->getCovariance().matrix());
 	BOOST_CHECK_EQUAL(c3->getDirection().matrix(), c3_res->getDirection().matrix());
 	BOOST_CHECK_EQUAL(c3->getReference().matrix(), c3_res->getReference().matrix());
 
 	slam3d::PositionConstraint::Ptr c4(new slam3d::PositionConstraint("S4", slam3d::Position::Identity(), slam3d::Covariance<3>::Identity(), slam3d::Transform::Identity()));
-	slam3d::PositionConstraint::Ptr c4_res = addAndGetConstraint(graph, c4, 1, 2, "S4");
+	slam3d::PositionConstraint::Ptr c4_res = addAndGetConstraint(graph, c4, 1, 2);
 	BOOST_CHECK_EQUAL(c4->getCovariance().matrix(), c4_res->getCovariance().matrix());
 	BOOST_CHECK_EQUAL(c4->getPosition().matrix(), c4_res->getPosition().matrix());
 	BOOST_CHECK_EQUAL(c4->getSensorPose().matrix(), c4_res->getSensorPose().matrix());
 
 	slam3d::OrientationConstraint::Ptr c5(new slam3d::OrientationConstraint("S5", slam3d::Quaternion::Identity(), slam3d::Covariance<3>::Identity(), slam3d::Transform::Identity()));
-	slam3d::OrientationConstraint::Ptr c5_res = addAndGetConstraint(graph, c5, 1, 2, "S5");
+	slam3d::OrientationConstraint::Ptr c5_res = addAndGetConstraint(graph, c5, 1, 2);
 	BOOST_CHECK_EQUAL(c5->getCovariance().matrix(), c5_res->getCovariance().matrix());
 	BOOST_CHECK_EQUAL(c5->getOrientation().matrix(), c5_res->getOrientation().matrix());
 	BOOST_CHECK_EQUAL(c5->getSensorPose().matrix(), c5_res->getSensorPose().matrix());
