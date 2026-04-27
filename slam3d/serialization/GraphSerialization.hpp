c40cc39f0e33aa1bf66f08ae76ac54c394ef39b4
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2026-04-27 13:47:36 +0200

    Indentation in public header files.

--- slam3d/serialization/GraphSerialization.hpp
+++ slam3d/serialization/GraphSerialization.hpp
@@ -3,37 +3,37 @@
 #include <string>
 #include <functional>
 
-#include "../core/Graph.hpp"
+#include <slam3d/core/Graph.hpp>
 
 
+namespace slam3d
+{
+	class GraphSerialization
+	{
+	public:
 
-namespace slam3d {
+		enum CloudMode {PORTABLE, BINARY, SKIP};
 
-class GraphSerialization {
- public:
+		/**
+		 * @brief saves the graph to a folder
+		 *
+		 * @param graph the slam3d graph
+		 * @param targetfolder folder to save the graph to
+		 * @param status callback to give the processing status: void (current coud, total clouds)
+		 * @param binaryClouds wheter to use text archives if binary
+		 */
+		static bool toFolder(Graph& graph, const std::string& targetfolder, const std::string &graphfile, std::function<void(size_t,size_t)> status = nullptr, const CloudMode &cloudmode = PORTABLE);
 
-    enum CloudMode {PORTABLE, BINARY, SKIP};
+		/**
+		 * @brief restores the graph from folder
+		 *
+		 * @param graph the slam3d graph
+		 * @param targetfolder folder to load the graph from
+		 * @param status callback to give the processing status: void (current coud, total clouds)
+		 * @param binaryClouds wheter to use text archives if binary
+		 */
+		static bool fromFolder(Graph* graph, const std::string& targetfolder, const std::string &graphfile, std::function<void(size_t,size_t)> status = nullptr, const CloudMode &cloudmode = PORTABLE);
 
-    /**
-     * @brief saves the graph to a folder
-     *
-     * @param graph the slam3d graph
-     * @param targetfolder folder to save the graph to
-     * @param status callback to give the processing status: void (current coud, total clouds)
-     * @param binaryClouds wheter to use text archives if binary
-     */
-    static bool toFolder(Graph& graph, const std::string& targetfolder, const std::string &graphfile, std::function<void(size_t,size_t)> status = nullptr, const CloudMode &cloudmode = PORTABLE);
-
-    /**
-     * @brief restores the graph from folder
-     *
-     * @param graph the slam3d graph
-     * @param targetfolder folder to load the graph from
-     * @param status callback to give the processing status: void (current coud, total clouds)
-     * @param binaryClouds wheter to use text archives if binary
-     */
-    static bool fromFolder(Graph* graph, const std::string& targetfolder, const std::string &graphfile, std::function<void(size_t,size_t)> status = nullptr, const CloudMode &cloudmode = PORTABLE);
-
-};
+	};
 
 }  // namespace slam3d
